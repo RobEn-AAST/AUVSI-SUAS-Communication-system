39e1d2a3dcc87896f4da259e854391b9f96699ae
mhwahdab <moustafawahdan55@gmail.com>
2022-04-26 15:45:13 +0200

    some cleanup to the module

--- Examples/RSA.py
+++ Examples/RSA.py
@@ -1,39 +1,30 @@
-import ast
-from socket import MsgFlag
-from Crypto.PublicKey import RSA
-from Crypto.Cipher import PKCS1_OAEP
-import binascii
+from Cryptodome.PublicKey.RSA import generate as generate_keys
+from Cryptodome.Cipher.PKCS1_OAEP import new as OAEP 
 
-from matplotlib.pyplot import cla
- 
-class RSA_ENCRYPTION():
 
-    def __init__(self,
-                file_path_to_be_encrypted)-> None:
+class RSA_DECRYPTO():
 
-        
-        keyPair = RSA.generate(3072)
-        pubKey = keyPair.publickey()
-        file = open(file_path_to_be_encrypted,"rb")#reading in binary
-        msg = file.read()
-        file.close()
-
-        self.msg = msg
-        self.pubKey = pubKey
-        self.keyPair = keyPair
-
-    def Public_key(self):
-        return (f"Public key:  (n={hex(self.pubKey.n)}, e={hex(self.pubKey.e)})")
-
-    def Private_Key(self):
-        return (f"Private key: (n={hex(self.pubKey.n)}, d={hex(self.keyPair.d)})")
+    def __init__(self)-> None:
+        key = generate_keys(3072)
+        private = key.export_key()
+        self.__public = key.public_key().export_key()
+        self.__decrypto = OAEP(private)
     
-    def Encrypted_Data(self):
-        encryptor = PKCS1_OAEP.new(self.pubKey)
-        encrypted = encryptor.encrypt(self.msg)
-        return encrypted
+    def get_public_key(self):
+        return self.__public
 
-    def Decrypted_Data(self,encrypted):
-        decryptor =  PKCS1_OAEP.new(self.keyPair)
-        decrypted = decryptor.decrypt(ast.literal_eval(str(encrypted)))
-        return decrypted
+    def decrypt(self, cipher):
+        message = self.__decrypto.decrypt(ciphertext= cipher)
+        return message
+    
+class RSA_ENCRYPTO():
+    def __init__(self, public)-> None:
+        self.__public = public
+        self.__encrypto = OAEP(public)
+        
+    def get_public_key(self):
+        return self.__public
+    
+    def encrypt(self, message):
+        cipher = self.__encrypto.encrypt(message= message)
+        return cipher
