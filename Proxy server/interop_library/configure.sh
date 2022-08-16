e737667a9359524fef12d39ab7258d988a8fe48b
mhwahdan <moustafawahdan55@gmail.com>
2022-08-16 05:50:41 +0200

    Added submodules integration

--- Proxy server/interop_library/configure.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-# Configures the Interop Client for the Docker image build.
-
-CLIENT=$(dirname ${BASH_SOURCE[0]})
-
-export PYTHONPATH=$CLIENT
-source ${CLIENT}/venv/bin/activate
